fb683adc991c6f9ec03916f3117ec57286e69e41
MiguelFigueira <miguel.figueira@stockonspot.com>
2025-11-09 09:32:32 +0000

    removed ton libraries

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -7,7 +7,6 @@ import {
   useLaunchParams,
   useSignal,
 } from '@telegram-apps/sdk-react';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 
 import { ErrorBoundary } from '@/components/ErrorBoundary';
@@ -29,8 +28,7 @@ function RootInner({ children }: PropsWithChildren) {
   }, [initDataUser]);
 
   return (
-    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
-      <AppRoot
+    <AppRoot
         appearance={isDark ? 'dark' : 'light'}
         platform={
           ['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'
@@ -38,7 +36,6 @@ function RootInner({ children }: PropsWithChildren) {
       >
         {children}
       </AppRoot>
-    </TonConnectUIProvider>
   );
 }
 
